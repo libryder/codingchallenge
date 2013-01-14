240d146980eb87f9befabcb8c28e903e9c16b715
David Ryder <david@libryder.com>
2013-01-13 22:08:23 -0800

    moving some view logic into a presenter

---
+++ app/presenters/solution_presenter.rb
@@ -0,0 +1,21 @@
+class SolutionPresenter < BasePresenter
+  presents :solution
+          
+  def up_vote_arrow
+    link_to('', challenge_solution_up_vote_path(solution.challenge, solution), class: "up icon-arrow-up vote-arrow #{up_arrow_class}")
+  end
+
+  def down_vote_arrow
+    link_to('', challenge_solution_down_vote_path(solution.challenge, solution), class: "down icon-arrow-down vote-arrow #{down_arrow_class}")
+  end
+
+private
+
+  def down_arrow_class
+    current_user.voted_down_on?(solution) ? 'icon-circle-arrow-down' : nil
+  end
+
+  def up_arrow_class
+    current_user.voted_up_on?(solution) ? 'icon-circle-arrow-up' : nil
+  end
+end
