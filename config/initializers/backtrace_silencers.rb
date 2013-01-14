b18c50cf32c647ed296ca7ff640d181c3b4d1d01
David Ryder <david@libryder.com>
2013-01-14 15:02:30 -0800

    adding prettify syntax highlighter

--- config/initializers/backtrace_silencers.rb
+++ config/initializers/backtrace_silencers.rb
@@ -5,3 +5,5 @@
 
 # You can also remove all the silencers if you're trying to debug a problem that might stem from framework code.
 # Rails.backtrace_cleaner.remove_silencers!
+# encoding: UTF-8
+# config/initializers/char_converter.rb
