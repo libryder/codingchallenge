893f9446129faf33b9a45bbb7c981a4a039c7e0d
David Ryder <david@libryder.com>
2013-01-15 15:30:51 -0800

    adding a title to solutions and redoing the layout

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130115174148) do
+ActiveRecord::Schema.define(:version => 20130115215316) do
 
   create_table "challenges", :force => true do |t|
     t.text     "gist_url"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(:version => 20130115174148) do
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
     t.text     "source"
+    t.text     "title"
   end
 
   create_table "users", :force => true do |t|
@@ -46,6 +47,9 @@ ActiveRecord::Schema.define(:version => 20130115174148) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.text     "roles"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
