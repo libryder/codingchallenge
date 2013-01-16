b884c7f6d73fba12bfbd1da3a63bc25aed245723
David Ryder <david@libryder.com>
2013-01-16 11:25:34 -0800

    adding a little more visual pleasure

--- app/presenters/solution_presenter.rb
+++ app/presenters/solution_presenter.rb
@@ -9,6 +9,22 @@ class SolutionPresenter < BasePresenter
     link_to('', challenge_solution_down_vote_path(solution.challenge, solution), id: "down_vote_#{solution.id}", class: "down icon-arrow-down vote-arrow #{down_arrow_class}")
   end
 
+  def vote_cell
+    content_tag(:div, class: 'vote-cell cell', rel: solution.id) do
+      content_tag(:div, class: 'arrows') do
+        if user_signed_in?
+          up_vote_arrow +
+          tag('br') +
+          down_vote_arrow
+        end
+      end +
+      content_tag(:div, class: 'vote-count') do
+        content_tag(:div, class: 'label') { 'votes' } +
+        content_tag(:div, class: 'count') { solution.positives.size }
+      end
+    end
+  end
+
 private
 
   def down_arrow_class
