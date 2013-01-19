79525d190091cab656e26faad177b6fd697a74a0
David Ryder <david@libryder.com>
2013-01-18 16:12:10 -0800

    adding activeadmin auth overrides

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,14 @@ class ApplicationController < ActionController::Base
     klass ||= "#{object.class}Presenter".constantize
     klass.new(object, view_context)
   end
+
+  def authenticate_admin_user!
+    render_403 and return if user_signed_in? && !current_user.admin?
+    authenticate_user!
+  end
+  
+  def current_admin_user
+    return nil if user_signed_in? && !current_user.admin?
+    current_user
+  end
 end
