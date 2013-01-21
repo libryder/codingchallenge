a68c3b21c555381e2846b3b49e133a8a357565f6
David Ryder <david@libryder.com>
2013-01-20 19:13:09 -0800

    Adding a validator to challenges
    We also don't want to show the countdown if the challenge is expired so this commit addresses that as well.

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,8 +1,26 @@
+class ChallengeExpiry < ActiveModel::Validator
+  attr_accessor :record
+
+  def validate(record)
+    if record.expired?
+      record.errors[:Challenge] << 'Challenge has expired.'
+    end
+  end
+end
+
 class Challenge < ActiveRecord::Base
-  attr_accessible :description, :down_votes, :gist_url, :up_votes, :title, :source, :solutions_attributes
+  include ActiveModel::Validations
+  validates_with ChallengeExpiry
+
+  attr_accessible :description, :down_votes, :gist_url, :up_votes, :title, :source, :solutions_attributes, :created_at
 
   has_many :solutions, dependent: :destroy
   validates :description, presence: true
 
   accepts_nested_attributes_for :solutions, allow_destroy: true
+  
+  def expired?
+    elapsed = Time.now.to_i - created_at.to_i
+    elapsed > 7.days.to_i
+  end
 end
